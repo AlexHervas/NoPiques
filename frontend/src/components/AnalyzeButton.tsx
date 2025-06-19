3b78200fee1f5e8137311c872520676b108579a3
Alejandro Hervás <alesepto15@gmail.com>
2025-06-19 18:48:20 +0200

    feat: enhance AnalyzeButton and MessageInput components with improved styles and structure

--- frontend/src/components/AnalyzeButton.tsx
+++ frontend/src/components/AnalyzeButton.tsx
@@ -1,3 +1,5 @@
+import { ShieldCheck } from "lucide-react";
+
 type Props = {
   onclick: () => void;
   disabled?: boolean;
@@ -8,10 +10,11 @@ export default function AnalyzeButton({ onclick, disabled }: Props) {
     <button
       onClick={onclick}
       disabled={disabled}
-      className={`w-full h-12 bg-indigo-600 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition-colors duration-200 ${
+      className={`cursor-pointer w-full flex items-center justify-center gap-2 h-12 px-6 py-3 bg-indigo-600 text-white text-sm font-medium rounded-xl shadow-md transition-transform duration-200 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:scale-[0.98] ${
         disabled ? "opacity-50 cursor-not-allowed" : ""
       }`}
     >
+      <ShieldCheck size={25} />
       Analizar mensaje
     </button>
   );
--- frontend/src/components/messageInput.tsx
+++ frontend/src/components/messageInput.tsx
@@ -5,11 +5,20 @@ type Props = {
 
 export default function MessageInput({ message, onChange }: Props) {
   return (
-    <textarea
-      value={message}
-      onChange={(e) => onChange(e.target.value)}
-      className="w-full h-40 p-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-400 resize-none"
-      placeholder="Pega aquí el mensaje que quieres analizar..."
-    />
+    <div className="space-y-2">
+      <label
+        htmlFor="message"
+        className="block text-sm font-medium text-gray-700"
+      >
+        Mensaje a analizar:
+      </label>
+      <textarea
+        id="message"
+        value={message}
+        onChange={(e) => onChange(e.target.value)}
+        className="w-full h-40 p-4 border border-gray-300 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 resize-none bg-white"
+        placeholder="Pega aquí el mensaje que quieres analizar..."
+      />
+    </div>
   );
 }
